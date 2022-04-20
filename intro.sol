3e2412c5d579e62d33a5594f8f719c9334923dc6
kaustubh thakur <93928807+kaustubhthakur@users.noreply.github.com>
2022-04-20 23:51:37 +0530

    Add files via upload

---
+++ intro.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Property 
+{
+    int public value;
+
+    function setValue(int _value) public 
+    {
+        value = _value;
+    }
+}
